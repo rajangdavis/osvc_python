f56ca890f205b33b3c23693b17680e8df9d589aa
Rajan Davis <rajangdavis@gmail.com>
2017-11-25 21:23:07 -0800

    Coverage should be fixed for now, testing to see if the CI build will send the info to CC correctly

--- osc_python/test_osc_python_client.py
+++ osc_python/test_osc_python_client.py
@@ -12,18 +12,28 @@ class TestOSCPythonClient(unittest.TestCase):
 			interface=env('OSC_SITE')
 		)
 	
+	def test_init(self):
+		self.assertEquals(env('OSC_ADMIN'),self.rn_client.username)
+		self.assertEquals(env('OSC_PASSWORD'),self.rn_client.password)
+		self.assertEquals(env('OSC_SITE'),self.rn_client.interface)
+		self.assertEquals('v1.3',self.rn_client.version)
+		self.assertTrue(self.rn_client.ssl_verify)
+		self.assertFalse(self.rn_client.rule_suppression)
+		self.assertFalse(self.rn_client.demo_site)
+
 	def test_change_version(self):
 		self.rn_client.change_version('v1.4')
-		self.assertEquals(self.rn_client.version,'v1.4')
+		self.assertEquals('v1.4',self.rn_client.version)
 
 	def test_ssl_off(self):
 		self.rn_client.ssl_off()
-		self.assertEquals(self.rn_client.ssl_verify,False)
+		self.assertEquals(False,self.rn_client.ssl_verify)
 
 	def test_suppress_rules(self):
 		self.rn_client.suppress_rules()
-		self.assertEquals(self.rn_client.rule_suppression,True)
+		self.assertEquals(True,self.rn_client.rule_suppression)
 
 	def test_is_demo(self):
 		self.rn_client.is_demo()
-		self.assertEquals(self.rn_client.demo_site,True)
+		self.assertEquals(True,self.rn_client.demo_site)
+
