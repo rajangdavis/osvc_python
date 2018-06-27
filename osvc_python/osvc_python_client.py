95afcdcfd79f449d3d9cc21739bdf5fc33bf3ea4
rajangdavis <rajangdavis@gmail.com>
2018-06-26 19:57:42 -0700

    refactored the client class, will probably work on the config class next

--- osvc_python/osvc_python_client.py
+++ osvc_python/osvc_python_client.py
@@ -3,38 +3,43 @@ class OSvCPythonClient:
 		self.interface = kwargs.get('interface','')
 		self.username = kwargs.get('username','')
 		self.password = kwargs.get('password','')
-		self.version = self.set_version(kwargs)
-		self.no_ssl_verify = self.ssl_check(kwargs)
-		self.suppress_rules = self.rule_suppresion(kwargs)
-		self.demo_site = self.is_demo(kwargs)
-		self.access_token = self.access_token_check(kwargs)
+		self.version = self.__generic_setter("version", kwargs)
+		self.no_ssl_verify = self.__generic_setter("no_ssl_verify", kwargs)
+		self.suppress_rules = self.__generic_setter("suppress_rules", kwargs)
+		self.demo_site = self.__generic_setter("demo_site", kwargs)
+		self.access_token = self.__generic_setter("access_token", kwargs)
 
-	def set_version(self,kwargs):
-		if 'version' in kwargs:
-			return kwargs.get('version')
-		else:
-			return 'v1.3'
+	def __generic_setter(self,prop,kwargs):
+		props_to_set = {
+			"version" : {
+				"conditional" : 'version' in kwargs,
+				"return_if_true" : kwargs.get('version'),
+				"return_if_false" : 'v1.3' 
+			},
+			"no_ssl_verify" : {
+				"conditional" : 'no_ssl_verify' in kwargs and kwargs.get('no_ssl_verify') == True,
+				"return_if_true" : True,
+				"return_if_false" : False 
+			},
+			"suppress_rules" : {
+				"conditional" : 'suppress_rules' in kwargs and kwargs.get('suppress_rules') == True,
+				"return_if_true" : True,
+				"return_if_false" : False 
+			},
+			"demo_site" : {
+				"conditional" : 'demo_site' in kwargs and kwargs.get('demo_site') == True,
+				"return_if_true" : True,
+				"return_if_false" : False 
+			},
+			"access_token" : {
+				"conditional" : 'access_token' in kwargs and len(kwargs.get('access_token')) > 0,
+				"return_if_true" : kwargs.get('access_token'),
+				"return_if_false" : '' 
+			},
+		}
 
-	def ssl_check(self, kwargs):
-		if 'no_ssl_verify' in kwargs and kwargs.get('no_ssl_verify') == True:
-			return True
+		prop_to_set = props_to_set[prop]
+		if prop_to_set["conditional"]:
+			return prop_to_set["return_if_true"]
 		else:
-			return False
-
-	def rule_suppresion(self, kwargs):
-		if 'suppress_rules' in kwargs and kwargs.get('suppress_rules') == True:
-			return True
-		else:
-			return False
-
-	def is_demo(self, kwargs):
-		if 'demo_site' in kwargs and kwargs.get('demo_site') == True:
-			return True
-		else:
-			return False
-
-	def access_token_check(self, kwargs):
-		if 'access_token' in kwargs and len(kwargs.get('access_token')) > 0:
-			return kwargs.get('access_token')
-		else:
-			return ''
+			return prop_to_set["return_if_false"]
