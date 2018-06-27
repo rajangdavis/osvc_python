e96c3abc225a979b9ddd0fadcf740baba0a189aa
rajangdavis <rajangdavis@gmail.com>
2018-06-26 17:24:07 -0700

    tested out the connect class, code sucks, but my tests pass for both python 2 and 3. going to try to get queryResults going next[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D

--- osvc_python/osvc_python_connect.py
+++ osvc_python/osvc_python_connect.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import base64
 from requests.auth import HTTPBasicAuth
 
 class OSvCPythonConnect:
@@ -11,16 +12,22 @@ class OSvCPythonConnect:
 		client = self.__check_client(kwargs)
 		get_url = self.__url_format(kwargs)
 		headers = self.__headers_check(kwargs)
+		download = self.__download_check(kwargs)
 		response = requests.get(get_url,
 					auth=(client.username,client.password),
 					headers=headers,
-					verify=not client.no_ssl_verify)
-		return self.__print_response(response,kwargs)
+					verify=not client.no_ssl_verify,
+					stream=download["stream"])
+		if download["stream"] == True:
+			return self.__download_file(response,download)
+		else:
+			return self.__print_response(response,kwargs)
 
-	def post(self,resource_url,json_data={}):
+	def post(self,**kwargs):
 		client = self.__check_client(kwargs)
-		json_data = json.dumps(json_data)
-		post_url = self.__url_format(resource_url)
+		json_data = json.dumps(self.__upload_check(kwargs))
+		post_url = self.__url_format(kwargs)
+		headers = self.__headers_check(kwargs)
 		response = requests.post(post_url,
 					 auth=(client.username,client.password),
 					 data=json_data,
@@ -28,11 +35,12 @@ class OSvCPythonConnect:
 					 verify=not client.no_ssl_verify)
 		return self.__print_response(response,kwargs)
 
-	def patch(self,resource_url,json_data={}):
+	def patch(self,**kwargs):
 		client = self.__check_client(kwargs)
-		json_data = json.dumps(json_data)
-		patch_url = self.__url_format(resource_url)
-		headers = self.__headers_check(patch=True)
+		json_data = json.dumps(self.__upload_check(kwargs))
+		patch_url = self.__url_format(kwargs)
+		kwargs['patch']=True
+		headers = self.__headers_check(kwargs)
 		response = requests.post(patch_url,
 				     auth=(client.username,client.password),
 				     data=json_data,
@@ -40,21 +48,35 @@ class OSvCPythonConnect:
 				     verify=not client.no_ssl_verify)
 		return self.__print_response(response,kwargs)
 
-	def delete(self,resource_url):
+	def delete(self,**kwargs):
 		client = self.__check_client(kwargs)
-		delete_url = self.__url_format(resource_url)
+		delete_url = self.__url_format(kwargs)
+		headers = self.__headers_check(kwargs)
 		response = requests.delete(delete_url,
 						headers=headers,
 						auth=(client.username,client.password),
 						verify=not client.no_ssl_verify)
 		return self.__print_response(response,kwargs)
+
+	def options(self,**kwargs):
+		client = self.__check_client(kwargs)
+		options_url = self.__url_format(kwargs)
+		headers = self.__headers_check(kwargs)
+		return requests.options(options_url,
+						headers=headers,
+						auth=(client.username,client.password),
+						verify=not client.no_ssl_verify).headers
 	
 	# Private Methods
-	# This needs to be updated to throw exceptions
-	# When things aren't correct
+	def __json_check(self,kwargs):
+		if "json" in kwargs:
+			return kwargs.get("json")
+		else:
+			return {}
+
 	def __check_client(self,kwargs):
-		if 'client' in kwargs:
-			client = kwargs.get('client')
+		if "client" in kwargs:
+			client = kwargs.get("client")
 		else:
 			raise Exception("Client must be defined")
 
@@ -67,12 +89,12 @@ class OSvCPythonConnect:
 		return client
 		
 	def __url_format(self,kwargs):
-		if 'url' in kwargs:
-			resource_url = kwargs.get('url')
+		if "url" in kwargs:
+			resource_url = kwargs.get("url")
 		else:
-			resource_url = ''
-		client = kwargs.get('client')
-		cust_or_demo = 'rightnowdemo' if client.demo_site is True else 'custhelp'  
+			resource_url = ""
+		client = kwargs.get("client")
+		cust_or_demo = "rightnowdemo" if client.demo_site is True else "custhelp"  
 		url = "https://{0}.{1}.com/services/rest/connect/{2}/{3}".format(
 			client.interface,cust_or_demo,client.version,resource_url)
 		return url
@@ -80,32 +102,94 @@ class OSvCPythonConnect:
 	def __headers_check(self,kwargs):
 		client = self.__check_client(kwargs)
 		headers = {}
-		if 'patch' in kwargs:
-			headers['X-HTTP-Method-Override'] = 'PATCH'
-		elif 'annotation' in kwargs:
+		if "patch" in kwargs:
+			headers["X-HTTP-Method-Override"] = "PATCH"
+		elif "annotation" in kwargs:
 			annotation = self.__annotation_check(kwargs)
-			headers['OSvC-CREST-Application-Context'] = kwargs.get('annotation')
-		elif 'exclude_null' in kwargs and kwargs.get('exclude_null') == True:
-			headers['prefer'] = "exclude-null-properties"
-		elif 'next_request' in kwargs and kwargs.get('next_request') > 0:
-			headers['osvc-crest-next-request-after'] = kwargs.get('next_request')
-		elif 'schema' in kwargs and kwargs.get('schema') == True:
-			headers['Accept'] = "application/schema+json"
-		elif 'utc_time' in kwargs and kwargs.get('utc_time') == True:
-			headers['OSvC-CREST-Time-UTC'] = kwargs.get('utc_time')
+			headers["OSvC-CREST-Application-Context"] = kwargs.get("annotation")
+		elif "exclude_null" in kwargs and kwargs.get("exclude_null") == True:
+			headers["prefer"] = "exclude-null-properties"
+		elif "next_request" in kwargs and kwargs.get("next_request") > 0:
+			headers["osvc-crest-next-request-after"] = kwargs.get("next_request")
+		elif "schema" in kwargs and kwargs.get("schema") == True:
+			headers["Accept"] = "application/schema+json"
+		elif "utc_time" in kwargs and kwargs.get("utc_time") == True:
+			headers["OSvC-CREST-Time-UTC"] = kwargs.get("utc_time")
 		elif client.suppress_rules is True:
-			headers['OSvC-CREST-Suppress-All'] = True
+			headers["OSvC-CREST-Suppress-All"] = True
 		return headers
 
 	def __annotation_check(self,kwargs):
-		annotation = kwargs.get('annotation')
+		annotation = kwargs.get("annotation")
 		if len(annotation) > 40:
 			raise Exception("Annotation cannot be greater than 40 characters")
 		else:
 			return annotation
 
 	def __print_response(self,response,kwargs):
-		if 'debug' in kwargs and kwargs.get('debug') == True:
+		if "debug" in kwargs and kwargs.get("debug") == True:
 			return response
 		else:
 			return response.json()
+
+	# Download Logic
+	def __download_check(self,kwargs):
+		if kwargs.get("url").find("?download") > -1:
+			resource_url = kwargs.get("url").replace("?download","")
+			file_data = self.get(client=kwargs.get("client"),url=resource_url)
+
+			file_name = self.__set_file_name(file_data)
+
+			return {"file_name" : file_name, "stream" : True}
+		else:
+			return {"file_name" : None,	"stream" : False }
+
+	# https://stackoverflow.com/a/16696317/2548452
+	# chunking downloads
+	def __download_file(self,response,download):
+		with open(download["file_name"], "wb") as f:
+			for chunk in response.iter_content(chunk_size=1024): 
+				if chunk: # filter out keep-alive new chunks
+					f.write(chunk)
+					f.flush()
+		return "Downloaded %s" % download["file_name"]
+
+	def __set_file_name(self,file_data):
+		if "items" in file_data:
+			return "downloadedAttachment.tgz"
+		else:
+			return file_data["fileName"]
+
+
+
+	# Upload Logic
+	def __upload_file_check(self,file_to_check):
+		error_issue = False
+		try:
+			file_to_upload = open(file_to_check, "rb")
+		except:
+			error_issue = True
+			pass
+		finally:
+			if error_issue == True:
+				raise Exception("Cannot locate file '%s'" % file_to_check)
+			else:
+				file_data = base64.b64encode(file_to_upload.read())
+				file_to_upload.close()
+				return file_data
+
+	def __upload_check(self,kwargs):
+		json_data = self.__json_check(kwargs)
+		if "files" in kwargs:
+			files_to_upload = kwargs.get("files")
+			json_data["fileAttachments"] = []
+			for file in files_to_upload:
+				encoded_string = self.__upload_file_check(file)
+				clean_file_name = file.replace("./","")
+				json_data["fileAttachments"].append({
+					"fileName" : clean_file_name, 
+					# https://stackoverflow.com/a/36212932/2548452
+					# Python 3 can't serialize bytes to json
+					"data" : encoded_string.decode("utf-8")
+				})
+		return json_data
--- osvc_python/test_osvc_python_connect.py
+++ osvc_python/test_osvc_python_connect.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, os
 from .osvc_python_connect import OSvCPythonConnect
 from .osvc_python_client import OSvCPythonClient
 from . import env
@@ -15,18 +15,164 @@ class TestOSvCPythonConnect(unittest.TestCase):
 		)
 	
 	def test_get(self):
+		
 		opc = OSvCPythonConnect()
+		
 		self.assertIsInstance(opc,OSvCPythonConnect)
-		response = opc.get(client=self.rn_client,url='answers',debug=True)
+
+		response = opc.get(
+			client=self.rn_client,
+			url='answers',
+			debug=True
+		)
+
 		self.assertEqual(response.status_code,200)
 		self.assertIsInstance(response.content,bytes)
 
-# test download of 1 file
-# test download of multiple files
-# test post request
-# test upload of 1 file
-# test upload of multiple files
-# test return error if file doesn't exist
-# test patch request
-# test delete request
-# test options request
+	def test_download_single_file(self):
+		
+		response = OSvCPythonConnect().get(
+			client=self.rn_client,
+			url='incidents/24898/fileAttachments/245?download',
+		)
+
+		assert response == "Downloaded haQE7EIDQVUyzoLDha2SRVsP415IYK8_ocmxgMfyZaw.png"
+		assert os.path.exists("./haQE7EIDQVUyzoLDha2SRVsP415IYK8_ocmxgMfyZaw.png") == 1
+		os.remove("./haQE7EIDQVUyzoLDha2SRVsP415IYK8_ocmxgMfyZaw.png")
+		assert os.path.exists("./haQE7EIDQVUyzoLDha2SRVsP415IYK8_ocmxgMfyZaw.png") == 0
+
+
+	def test_download_multiple_files(self):
+		
+		response = OSvCPythonConnect().get(
+			client=self.rn_client,
+			url='incidents/24898/fileAttachments?download',
+		)
+
+		assert response == "Downloaded downloadedAttachment.tgz"
+		assert os.path.exists("./downloadedAttachment.tgz") == 1
+		os.remove("./downloadedAttachment.tgz")
+		assert os.path.exists("./downloadedAttachment.tgz") == 0
+
+
+	def test_post(self):
+		
+		data = {
+			"primaryContact": {
+				"id": 2
+			},
+			"subject": "FishPhone not working"
+		}
+
+		response = OSvCPythonConnect().post(
+			client=self.rn_client,
+			url='incidents?expand=all',
+			json=data,
+			debug=True
+		)
+
+		self.assertEqual(response.status_code,201)
+		self.assertIsInstance(response.content,bytes)
+
+	def test_upload_one_file(self):
+		
+		data = {
+			"primaryContact": {
+				"id": 2
+			},
+			"subject": "FishPhone not working"
+		}
+
+		response = OSvCPythonConnect().post(
+			client=self.rn_client,
+			url='incidents?expand=all',
+			json=data,
+			debug=True,
+			files=["./setup.py"]
+		)
+
+		json_response = response.json()
+
+		self.assertEqual(len(json_response['fileAttachments']['items']),1)
+		self.assertEqual(response.status_code,201)
+		self.assertIsInstance(response.content,bytes)
+
+	def test_upload_multiple_files(self):
+		
+		data = {
+			"primaryContact": {
+				"id": 2
+			},
+			"subject": "FishPhone not working"
+		}
+
+		response = OSvCPythonConnect().post(
+			client=self.rn_client,
+			url='incidents?expand=all',
+			json=data,
+			debug=True,
+			files=["./setup.py","./LICENSE.txt"]
+		)
+
+		json_response = response.json()
+
+		self.assertEqual(len(json_response['fileAttachments']['items']),2)
+		self.assertEqual(response.status_code,201)
+		self.assertIsInstance(response.content,bytes)
+
+	def test_raise_exception_when_file_to_upload_does_not_exist(self):
+		
+		data = {
+			"primaryContact": {
+				"id": 2
+			},
+			"subject": "FishPhone not working"
+		}
+
+		def returnError(self):
+			return OSvCPythonConnect().post(
+				client=self.rn_client,
+				url='incidents?expand=all',
+				json=data,
+				debug=True,
+				files=["./non-existent"]
+			)
+
+		self.assertRaises(Exception, returnError)
+
+	def test_patch(self):
+		
+		data = {
+			"primaryContact": {
+				"id": 2
+			},
+			"subject": "FishPhone not working UPDATED"
+		}
+
+		response = OSvCPythonConnect().patch(
+			client=self.rn_client,
+			url='incidents/26277',
+			json=data,
+			debug=True
+		)
+
+		self.assertEqual(response.status_code,200)
+		self.assertIsInstance(response.content,bytes)
+
+	def test_delete(self):
+		response = OSvCPythonConnect().delete(
+			client=self.rn_client,
+			url='incidents/0',
+			debug=True
+		)
+
+		self.assertEqual(response.status_code,404)
+		self.assertIsInstance(response.content,bytes)
+
+	def test_options(self):
+		response = OSvCPythonConnect().options(
+			client=self.rn_client,
+			url='incidents'
+		)
+
+		self.assertEqual(response['OSvCStatus'],'200')
